eee797a4f6cf232875f35812dea76e10babd026c
Ezra Wu <ezra40907@gmail.com>
2023-06-02 13:08:06 +0800

    1. Add UpdateGameState to handle state update. 2. Add Event to emit state changed event.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,39 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    void Start()
+    public EGameState state;
+    public static event System.Action<EGameState> OnGameStateChanged;
+    void Awake()
     {
-        
+        Instance = this;
     }
-
-    void Update()
+    public void UpdateGameState(EGameState newState)
     {
-        
+        state =  newState;
+        switch (state)
+        {
+            case EGameState.Idle:
+                break;
+            case EGameState.Introduce:
+                break;
+            case EGameState.PutQuestion:
+                break;
+            case EGameState.AwaitAnswer:
+                break;
+            case EGameState.FinishAnser:
+                break;
+            case EGameState.ReviewAnser:
+                break;
+            case EGameState.Ending:
+                break;
+            default:
+                throw new System.ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+        OnGameStateChanged?.Invoke(newState);
+    }
+    private void Start()
+    {
+        UpdateGameState(EGameState.Idle);
     }
 }
 
