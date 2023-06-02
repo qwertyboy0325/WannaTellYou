47343624e20ad7686f4dac404d27c8a3173a539a
Ezra Wu <ezra40907@gmail.com>
2023-06-02 11:17:43 +0800

    1. add private selectedRelation var 2. add resetFlow function to reset gameflow. 3. add flow function. 4. delete RelationSet, and replaced it by Relation (abstract). 5. add assignRelationType to assign relation by selectedRelation enum.

--- Assets/Workflow.cs
+++ Assets/Workflow.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 public class Workflow : MonoBehaviour
 {
     // 0: Friend 1: Couple 2: Stranger -1: Not Selected , other number will ignore it.
-    public ERelation selectedRelation;
-    private Relation[] Relations;
+    private ERelation _selectedRelation;
+    public ERelation selectedRelation
+    {
+        set
+        {
+            _selectedRelation = value;
+            resetFlow();
+        }
+        get => _selectedRelation;
+    }
+    private Relation relation;
     public string[] questions;
+    public PlayerStatus player1, player2;
     void Start()
     {
         // init var
-        RelationSets relationSets = new RelationSets();
-        Relations = relationSets.relations;
+        // RelationSets relationSets = new RelationSets();
+        // relations = relationSets.relations;
+        player1 = new PlayerStatus();
+        player2 = new PlayerStatus();
 
         // init workflow status
         selectedRelation = ERelation.None;
@@ -22,12 +34,46 @@ public class Workflow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Code for express the flow (just for reference, dont use it.)
+    void flow()
+    {
+        assignRelationType();
+    }
+    void resetFlow()
+    {
+
+    }
+    void assignRelationType()
+    {
+        switch (selectedRelation)
+        {
+            case ERelation.None:
+                break;
+            case ERelation.Friend:
+                relation = new Friend();
+                break;
+            case ERelation.Couple:
+                relation = new Couple();
+                break;
+            case ERelation.Stranger:
+                relation = new Stranger();
+                break;
+        }
+    }
+    private void OnValidate()
+    {
+
     }
 }
 public class PlayerStatus
 {
+    public PlayerStatus()
+    {
 
+    }
 }
 public class Round
 {
